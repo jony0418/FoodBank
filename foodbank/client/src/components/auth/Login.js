7bfe7a30de528586579e91795c9f85762b076197
RodrigoRoL <rodrigo.roldanleyva@gmail.com>
2023-08-13 18:16:14 -0500

    linked the sign up (register) page with the log in
    Linked the sign up and register pages

--- foodbank/client/src/components/auth/Login.js
+++ foodbank/client/src/components/auth/Login.js
@@ -1,6 +1,8 @@
 'use client'
 
-import { Button, Checkbox, Flex, Text, FormControl, FormLabel, Heading, Input, Stack, Image, Box, Link} from '@chakra-ui/react'
+import { Button, Checkbox, Flex, Text, FormControl, FormLabel, Heading, Input, Stack, Image, Box, Link} from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom'
+
 
 export default function SplitScreen() {
   return (
@@ -30,7 +32,7 @@ export default function SplitScreen() {
           </Stack>
           <Box>
         New to us?{" "}
-        <Link color="teal.500" href="#">
+        <Link color="teal.500" href="#" as={ReactRouterLink} to= '/register'>
           Sign Up
         </Link>
       </Box>
@@ -38,11 +40,11 @@ export default function SplitScreen() {
       </Flex>
       <Flex flex={1}>
         <Image
-          alt={'Login Image'}
-          objectFit={'cover'}
-          src={
+           src={
             'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
           }
+          alt={'Login Image'}
+          objectFit={'cover'}
         />
       </Flex>
     </Stack>
