87b305dc3a419ffa03fba2422ea777df5b433b0a
Isa <iruizord@gmail.com>
2023-08-14 20:24:29 -0600

    fixing error in app.js, productlist shows now

--- foodbank/client/src/App.js
+++ foodbank/client/src/App.js
@@ -1,51 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ChakraProvider, ColorModeScript, CSSReset } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript, CSSReset } from '@chakra-ui/react';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import theme from './theme';
 import Dashboard from './components/admin/Dashboard';
-import productlist from './pages/productlist';
+import ProductList from './pages/productlist'; 
 
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
 });
-
 function App() {
   return (
     <ApolloProvider client={client}>
-  <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode="light" />
-    <CSSReset />
-      <Router>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
-        <Routes>
-          <Route 
-            path="/" 
-            element={<Login/>} 
-          />
-          <Route 
-            path="/register" 
-            element={<Register/>} 
-          />
-           <Route 
-            path="/dashboard" 
-            element={
-            <Dashboard/>} 
-          />
-             <Route 
-            path="/productlist" 
-            element={<productlist/>} 
-            />
-        </Routes>
-      </div>
-    </Router>
-  </ChakraProvider>
-  </ApolloProvider>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode="light" />
+        <CSSReset />
+        <Router>
+          <div className="flex-column justify-center align-center min-100-vh bg-primary">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/productlist" element={<ProductList />} />
+            </Routes>
+          </div>
+        </Router>
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
-
 export default App;
