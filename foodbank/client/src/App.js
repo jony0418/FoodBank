7b00ac445cd7f0797e2a0da460488341c1dae07c
RodrigoRoL <rodrigo.roldanleyva@gmail.com>
2023-08-14 20:29:26 -0500

    App.js
    Added the apollo client and apollo provider to the file

--- foodbank/client/src/App.js
+++ foodbank/client/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ChakraProvider, ColorModeScript, CSSReset } from '@chakra-ui/react'
-
-// Uncomment import statement below after building queries and mutations
-// import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import theme from './theme';
 import Dashboard from './components/admin/Dashboard';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
   <ChakraProvider theme={theme}>
     <ColorModeScript initialColorMode="light" />
     <CSSReset />
@@ -35,6 +39,7 @@ function App() {
       </div>
     </Router>
   </ChakraProvider>
+  </ApolloProvider>
   );
 }
 
