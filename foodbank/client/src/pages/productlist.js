527355ec5aac98eb126a0801e35b701b102d188b
Isa <iruizord@gmail.com>
2023-08-14 21:38:18 -0600

    working on table

--- foodbank/client/src/pages/productlist.js
+++ foodbank/client/src/pages/productlist.js
@@ -3,7 +3,6 @@ import {
     Table,
     Thead,
     Tbody,
-    Tfoot,
     Tr,
     Th,
     Td,
@@ -16,38 +15,34 @@ function productlist () {
     <div className='productlist'>
    <TableContainer>
   <Table variant='simple'>
-    <TableCaption>Imperial to metric conversion factors</TableCaption>
+    <TableCaption>Inventory</TableCaption>
     <Thead>
       <Tr>
-        <Th>To convert</Th>
-        <Th>into</Th>
-        <Th isNumeric>multiply by</Th>
+        <Th>Name</Th>
+        <Th>Quantity</Th>
+        {/* <Th isNumeric> */}
+        <Th>Measuring unit</Th>
+        <Th>Category</Th>
+        <Th>Family</Th>
       </Tr>
     </Thead>
     <Tbody>
       <Tr>
         <Td>inches</Td>
         <Td>millimetres (mm)</Td>
-        <Td isNumeric>25.4</Td>
+        <Td>25.4</Td>
       </Tr>
       <Tr>
         <Td>feet</Td>
         <Td>centimetres (cm)</Td>
-        <Td isNumeric>30.48</Td>
+        <Td>30.48</Td>
       </Tr>
       <Tr>
         <Td>yards</Td>
         <Td>metres (m)</Td>
-        <Td isNumeric>0.91444</Td>
+        <Td>0.91444</Td>
       </Tr>
     </Tbody>
-    <Tfoot>
-      <Tr>
-        <Th>To convert</Th>
-        <Th>into</Th>
-        <Th isNumeric>multiply by</Th>
-      </Tr>
-    </Tfoot>
   </Table>
 </TableContainer>
     </div> 
