594ed1f3b8e2faaaef952054059bbbd07421f78d
RodrigoRoL <rodrigo.roldanleyva@gmail.com>
2023-08-15 20:24:36 -0500

    auth.js, register.js and login.js changes
    -Created the auth.js file for the token
    -Added mutations and login code to the login.js file
    -added route to login page at register.js file

--- foodbank/client/src/components/auth/Login.js
+++ foodbank/client/src/components/auth/Login.js
@@ -1,22 +1,65 @@
 'use client'
 
 import { Button, Checkbox, Flex, Text, FormControl, FormLabel, Heading, Input, Stack, Image, Box, Link } from '@chakra-ui/react';
-import { Link as ReactRouterLink } from 'react-router-dom'
+import { Link as ReactRouterLink } from 'react-router-dom';
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { UserLogin } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const Login = (props) => {
+    const [userState, setuserState] = useState({ email: '', password: '' });
+    const [login, { error, data }] = useMutation(UserLogin);
+  
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+  
+      setuserState({
+        ...userState,
+        [name]: value,
+      });
+    };
+  
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+      console.log(userState);
+      try {
+        const { data } = await login({
+          variables: { ...userState },
+        });
+  
+        Auth.login(data.login.token);
+      } catch (e) {
+        console.error(e);
+      }
+  
+      setuserState({
+        email: '',
+        password: '',
+      });
+    };
 
 
-export default function SplitScreen() {
+//export default function SplitScreen() {
   return (
     <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
       <Flex p={8} flex={1} align={'center'} justify={'center'}>
         <Stack spacing={4} w={'full'} maxW={'md'}>
           <Heading fontSize={'2xl'}>Sign in to your account</Heading>
+          <form onSubmit={handleFormSubmit}>
           <FormControl id="email">
             <FormLabel>Email address</FormLabel>
-            <Input type="email" />
+            <Input type="email" 
+                    name="email"
+                    value={userState.email}
+                    onChange={handleChange}/>
           </FormControl>
           <FormControl id="password">
             <FormLabel>Password</FormLabel>
-            <Input type="password" />
+            <Input type="password" 
+                    name="password"
+                    value={userState.password}
+                    onChange={handleChange} />
           </FormControl>
           <Stack spacing={6}>
             <Stack
@@ -30,6 +73,7 @@ export default function SplitScreen() {
               Sign in
             </Button>
           </Stack>
+          </form>
           <Box>
             New to us?{" "}
             <Link color="teal.500" href="#" as={ReactRouterLink} to='/register'>
@@ -49,4 +93,7 @@ export default function SplitScreen() {
       </Flex>
     </Stack>
   )
-}
+//}
+};
+
+export default Login;
