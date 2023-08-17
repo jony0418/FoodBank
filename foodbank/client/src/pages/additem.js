dfaaa9abd57b36f9a7b6937014df5da32c6d483c
Isa <iruizord@gmail.com>
2023-08-17 12:23:56 -0600

    adding the button properly

--- foodbank/client/src/pages/additem.js
+++ foodbank/client/src/pages/additem.js
@@ -1,5 +1,6 @@
+
 import React, { useState } from 'react';
-import { Stack, InputGroup, Input, InputLeftAddon, Button } from '@chakra-ui/react';
+import { Stack, InputGroup, Input, InputLeftAddon, Button, Flex } from '@chakra-ui/react';
 
 function AddItem() {
   const inputLeftAddonStyle = {
@@ -26,24 +27,69 @@ function AddItem() {
     console.log('Input values:', inputValues);
   };
 
+  const buttonStyle = {
+    width: '100px', // Adjust the width as needed
+  };
+
   return (
     <Stack spacing={4}>
-      <InputGroup>
-        <InputLeftAddon style={inputLeftAddonStyle} children='id' />
-        <Input
-          placeholder='item id'
-          value={inputValues.id}
-          onChange={(e) => handleInputChange('id', e.target.value)}
-        />
-      </InputGroup>
+        <InputGroup>
+          <InputLeftAddon style={inputLeftAddonStyle} children='id' />
+          <Input
+            placeholder='item id'
+            value={inputValues.id}
+            onChange={(e) => handleInputChange('id', e.target.value)}
+          />
+        </InputGroup>
+
+        <InputGroup>
+          <InputLeftAddon style={inputLeftAddonStyle} children='name' />
+          <Input
+            placeholder='item name'
+            value={inputValues.id}
+            onChange={(e) => handleInputChange('id', e.target.value)}
+          />
+        </InputGroup>
+
+        <InputGroup>
+          <InputLeftAddon style={inputLeftAddonStyle} children='measuring units' />
+          <Input
+            placeholder='pieces/kg/etc'
+            value={inputValues.id}
+            onChange={(e) => handleInputChange('id', e.target.value)}
+          />
+        </InputGroup>
+
+        <InputGroup>
+          <InputLeftAddon style={inputLeftAddonStyle} children='category' />
+          <Input
+            placeholder='fruit/legume/etc'
+            value={inputValues.id}
+            onChange={(e) => handleInputChange('id', e.target.value)}
+          />
+        </InputGroup>
+
+        <InputGroup>
+          <InputLeftAddon style={inputLeftAddonStyle} children='family' />
+          <Input
+            placeholder='???'
+            value={inputValues.id}
+            onChange={(e) => handleInputChange('id', e.target.value)}
+          />
+        </InputGroup>
+
 
       {/* Repeat the above pattern for other input groups */}
       
-      <Button size="sm" colorScheme="blue" onClick={handleAddItem}>
-        Add Item
-      </Button>
+      <Flex justifyContent="center">
+        <Button size="sm" colorScheme="green" style={buttonStyle} onClick={handleAddItem}>
+          Add Item
+        </Button>
+      </Flex>
     </Stack>
   );
 }
 
 export default AddItem;
+
+
