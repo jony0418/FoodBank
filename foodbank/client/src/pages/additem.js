23ddb6e9ae72545c528760d17090d8b2aaa961c1
Isa <iruizord@gmail.com>
2023-08-17 12:03:21 -0600

    changing button size

--- foodbank/client/src/pages/additem.js
+++ foodbank/client/src/pages/additem.js
@@ -1,46 +1,3 @@
-// import React from 'react'
-// import { Stack, InputGroup, Input, InputLeftAddon } from '@chakra-ui/react';
-
-// function AddItem () {
-//     const inputLeftAddonStyle = {
-//       width: '150px', // Adjust the width as needed
-//     }
-
-// return ( 
-// <Stack spacing={4}>
-//   <InputGroup>
-//     <InputLeftAddon style={inputLeftAddonStyle} children='id' />
-//     <Input placeholder='item id' />
-//   </InputGroup>
-
-//   {/* If you add the size prop to `InputGroup`, it'll pass it to all its children. */}
-
-//   <InputGroup>
-//     <InputLeftAddon style={inputLeftAddonStyle} children='name' />
-//     <Input placeholder='item name' />
-//   </InputGroup>
-
-//   <InputGroup>
-//     <InputLeftAddon style={inputLeftAddonStyle} children='measuring units' />
-//     <Input placeholder='pieces/kg/etc' />
-//   </InputGroup>
-
-//   <InputGroup>
-//     <InputLeftAddon style={inputLeftAddonStyle} children='category' />
-//     <Input placeholder='fruit/legume/etc' />
-//   </InputGroup>
-
-//   <InputGroup>
-//     <InputLeftAddon style={inputLeftAddonStyle} children='family' />
-//     <Input placeholder='???' />
-//   </InputGroup>
-
-// </Stack>
-//     )}
-
-
-// export default AddItem;
-
 import React, { useState } from 'react';
 import { Stack, InputGroup, Input, InputLeftAddon, Button } from '@chakra-ui/react';
 
@@ -82,7 +39,7 @@ function AddItem() {
 
       {/* Repeat the above pattern for other input groups */}
       
-      <Button colorScheme="blue" onClick={handleAddItem}>
+      <Button size="sm" colorScheme="blue" onClick={handleAddItem}>
         Add Item
       </Button>
     </Stack>
