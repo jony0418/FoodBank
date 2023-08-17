6a3d3691271fe386eccbdc39850fcfe309de0eb8
Isa <iruizord@gmail.com>
2023-08-17 14:10:24 -0600

    editing table on productlist to lead to other pages

--- foodbank/client/src/pages/productlist.js
+++ foodbank/client/src/pages/productlist.js
@@ -1,62 +1,87 @@
-import React from 'react'
+import React from 'react';
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-  } from '@chakra-ui/react'
-  
-function ProductList () {
-    return ( 
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Button,
+  Flex
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
+function ProductList() {
+  return (
     <div className='productlist'>
-   <TableContainer>
-  <Table variant='simple'>
-    <TableCaption>Inventory</TableCaption>
-    <Thead>
-      <Tr>
-        <Th>Description</Th>
-        <Th>Id</Th>
-        <Th>Quantity</Th>
-        {/* <Th isNumeric> */}
-        <Th>Measuring unit</Th>
-        <Th>Category</Th>
-        <Th>Family</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      <Tr>
-        <Td>Apple </Td>
-        <Td>IdNumber </Td>
-        <Td>3</Td>
-        <Td>Pieces</Td>
-        <Td>Fruit</Td>
-        <Td>8</Td>
-      </Tr>
-      <Tr>
-        <Td>Rice</Td>
-        <Td>IdNumber</Td>
-        <Td>7</Td>
-        <Td>Pieces</Td>
-        <Td>Legumes</Td>
-        <Td>10</Td>
-      </Tr>
-      <Tr>
-        <Td>Canned beans</Td>
-        <Td>IdNumber</Td>
-        <Td>13</Td>
-        <Td>Pieces</Td>
-        <Td>Legumes</Td>
-        <Td>9</Td>
-      </Tr>
-    </Tbody>
-  </Table>
-</TableContainer>
-    </div> 
-    )
+      <TableContainer>
+        <Table variant='simple'>
+          <TableCaption>Inventory</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Description</Th>
+              <Th>Id</Th>
+              <Th>Quantity</Th>
+              <Th>Measuring unit</Th>
+              <Th>Category</Th>
+              <Th>Family</Th>
+              <Th>Modify Item</Th> {/* New column */}
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>Apple</Td>
+              <Td>IdNumber</Td>
+              <Td>3</Td>
+              <Td>Pieces</Td>
+              <Td>Fruit</Td>
+              <Td>8</Td>
+              <Td>
+                <Link to='/modifyitem'>Modify</Link>
+              </Td>
+            </Tr>
+            <Tr>
+              <Td>Rice</Td>
+              <Td>IdNumber</Td>
+              <Td>7</Td>
+              <Td>Pieces</Td>
+              <Td>Legumes</Td>
+              <Td>10</Td>
+              <Td>
+                <Link to='/modifyitem'>Modify</Link>
+              </Td>
+            </Tr>
+            <Tr>
+              <Td>Canned beans</Td>
+              <Td>IdNumber</Td>
+              <Td>13</Td>
+              <Td>Pieces</Td>
+              <Td>Legumes</Td>
+              <Td>9</Td>
+              <Td>
+                <Link to='/modifyitem'>Modify</Link>
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
+
+      
+      <Flex justifyContent='center' mt={4}>
+        <Button
+          as={Link}
+          to='/additem'
+          colorScheme='green'
+        >
+          Add New Item
+        </Button>
+      </Flex>
+    </div>
+  );
 }
 
+  
 export default ProductList;
+
