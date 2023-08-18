e66e1de50d3cef89a750f2832e899cca48de49c0
RodrigoRoL <rodrigo.roldanleyva@gmail.com>
2023-08-17 20:20:43 -0500

    foodbank.png and app.js
    deleted FoodBank.png and deleted createHttpLink from App.js

--- foodbank/client/src/App.js
+++ foodbank/client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ChakraProvider, ColorModeScript, CSSReset } from '@chakra-ui/react';
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
