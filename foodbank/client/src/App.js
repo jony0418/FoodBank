b1114e9bda827b43343d58fa2eb4fd079f0e6f87
Jonathan Becerra <78985884+jony0418@users.noreply.github.com>
2023-08-17 21:03:18 -0600

    Update App.js

--- foodbank/client/src/App.js
+++ foodbank/client/src/App.js
@@ -5,13 +5,17 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import ProductList from './pages/productlist'; 
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import theme from "./theme";
+import Dashboard from "./components/admin/Dashboard";
 import BoM from "./pages/DistributionRequest";
 import AddItem from './pages/additem';
 import ModifyItem from './pages/modifyitem';
 
-//const httpLink = createHttpLink({
-//  uri: '/graphql',
-//});
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
