a2b50e7c212a4f2f93b4908cea5119aecb227de6
Andrea Ceron <andreacmz1996@gmail.com>
2023-08-20 15:20:24 -0500

    Update FormDistribition.js

--- foodbank/client/src/hooks/FormDistribition.js
+++ foodbank/client/src/hooks/FormDistribition.js
@@ -12,33 +12,46 @@ import {
   NumberDecrementStepper,
   Heading,
 } from "@chakra-ui/react";
+// import { useEffect } from "react";
 
 function DistributionRequest() {
-  const [billOfMaterial, setBillofMaterial] = useState([]);
+  // const [billOfMaterial, initialMaterial] = useState([]);
+
+  // useEffect(() => {
+  //   initialDataAPI()
+  //     .then((data) => {
+  //       initialMaterial(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data", error);
+  //     });
+  // }, []);
 
   return (
     <FormControl isRequired>
-      <Heading as="h1" size="l">
-        Distribution Form
-      </Heading>
-      <FormLabel padding={1}>Item</FormLabel>
-      <Select padding={1} placeholder="Select Item">
-        <option>Can Corn</option>
-        <option>Oil</option>
-      </Select>
-      <FormLabel padding={1}>Quantity</FormLabel>
-      <NumberInput padding={1} max={50} min={1}>
-        <NumberInputField />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInput>
-      <FormLabel padding={1}>Food Distribution</FormLabel>
-      <Select padding={1} placeholder="Select One">
-        <option>Intake</option>
-        <option>Output</option>
-      </Select>
+      <form>
+        <Heading as="h1" size="l">
+          Distribution Form
+        </Heading>
+        <FormLabel padding={1}>Item</FormLabel>
+        <Select padding={1} placeholder="Select Item">
+          <option>Can Corn</option>
+          <option>Oil</option>
+        </Select>
+        <FormLabel padding={1}>Quantity</FormLabel>
+        <NumberInput padding={1} max={50} min={1}>
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+        <FormLabel padding={1}>Food Distribution</FormLabel>
+        <Select padding={1} placeholder="Select One">
+          <option>Intake</option>
+          <option>Output</option>
+        </Select>
+      </form>
     </FormControl>
   );
 }
