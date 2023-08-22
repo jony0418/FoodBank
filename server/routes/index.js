6e187f44a2fdfa10c86868fc84cb2b729d53bdfd
SV <72838881+santis1001@users.noreply.github.com>
2023-08-22 17:13:18 -0600

    fix api

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const distributiRonoutes = require('./distribution');
 const inventorytRoutes = require('./inventory');
 
-router.use('/distribution', distributiRonoutes);
-router.use('/inventory', inventorytRoutes);
+router.use('/api/distribution', distributiRonoutes);
+router.use('/api/inventory', inventorytRoutes);
 
 module.exports = router;
