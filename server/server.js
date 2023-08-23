582b10c065319450f91b842556a9580e3573c983
SV <72838881+santis1001@users.noreply.github.com>
2023-08-22 18:53:04 -0600

    static

--- server/server.js
+++ server/server.js
@@ -24,11 +24,11 @@ app.use(express.json());
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/','index.html'));
 });
 
 
